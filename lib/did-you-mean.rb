202cdab2c1b38b4b1e15c662f7beefb845b30d47
Leonardo Bessa <leobessa@gmail.com>
2010-04-12 01:39:31 -0300

    adding method search support in method missing

--- lib/did-you-mean.rb
+++ lib/did-you-mean.rb
@@ -3,20 +3,35 @@ require 'levenshtein'
 
 module DidYouMean
   def self.included(base)
-      # base.extend SomeOtherModule
-      base.class_eval do
-          
-          def methods_with_a_search
-            methods_array = methods_with_no_search
-            class << methods_array
-              def search(query)
-                self.sort_by {|name| Levenshtein.normalized_distance(name.to_s, query.to_s) }
-              end
-            end
-            methods_array
+    base.class_eval do
+      def methods_with_a_search
+        methods_array = methods_with_no_search
+        class << methods_array
+          def search(query)
+            self.sort_by {|name| Levenshtein.normalized_distance(name.to_s, query.to_s) }
           end
-          alias_method :methods_with_no_search, :methods
-          alias_method :methods, :methods_with_a_search
+        end
+        methods_array
       end
-  end 
-end
+      alias_method :methods_with_no_search, :methods
+      alias_method :methods, :methods_with_a_search
+
+      unless method_defined? :method_missing
+        def method_missing(meth, *args, &block); super; end
+      end
+      alias_method :method_missing_without_method_search, :method_missing
+      alias_method :method_missing, :method_missing_with_method_search
+
+    end
+
+  end
+  def method_missing_with_method_search(name,*args,&block)
+    unless (self.respond_to? name)
+      best_match = self.methods.search(name).first
+      $stdout.print "Did you mean #{self.class}.#{best_match} (yes/no)?\n"
+      response = $stdin.gets
+      return self.send(best_match,*args,&block) if /yes/ =~ response
+    end
+    method_missing_without_method_search name,*args,&block
+  end
+end 
--- spec/did-you-mean_spec.rb
+++ spec/did-you-mean_spec.rb
@@ -1,24 +1,45 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "DidYouMean" do
-  
-  before(:all) do
-    Object.class_eval { include(DidYouMean) }
+
+  context "methods search" do
+    subject do
+      Class.new{include(DidYouMean)}.new
+    end
+
+    it "should search methods by textual proximity" do
+      def subject.foo; end
+      subject.methods.search(:fo).first.should == "foo"
+    end
+
+    it "should put javascript_driver method in first place when searching by default_javascript_driver" do
+      def subject.javascript_driver; end
+      subject.methods.search(:default_javascript_driver).first.should == "javascript_driver"
+    end
+
   end
-  
-  after(:all) do
-    Object.class_eval { include(DidYouMean) }
+
+  context "method missing" do
+    subject do
+      Class.new{include(DidYouMean)}.new
+    end
+
+    it "should suggest methods by textual proximity and call it if develepor types yes" do
+      def subject.foo; end
+      $stdout.should_receive(:print).with("Did you mean #{subject.class}.foo (yes/no)?\n")
+      $stdin.should_receive(:gets).and_return('yes')
+      subject.should_receive(:foo)
+      subject.fo
+    end
+    
+    it "should suggest methods by textual proximity and dont call it if develepor types no" do
+      def subject.foo; end
+      $stdout.should_receive(:print).with("Did you mean #{subject.class}.foo (yes/no)?\n")
+      $stdin.should_receive(:gets).and_return('no')
+      subject.should_not_receive(:foo)
+      lambda {subject.fo}.should raise_error(NoMethodError)      
+    end
+
   end
-  
-  subject { Object.new }
-  it "should search methods by textual proximity" do
-    def subject.foo; end
-    subject.methods.search(:fo).first.should == "foo"
-  end
-  
-  it "should put javascript_driver method in first place when searching by default_javascript_driver" do
-    def subject.javascript_driver; end
-    subject.methods.search(:default_javascript_driver).first.should == "javascript_driver"
-  end
-  
+
 end
