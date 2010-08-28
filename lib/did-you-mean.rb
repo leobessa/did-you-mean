5d7e05ce71625d3fb2329a2cbaad12d33991a02c
Leonardo Bessa <leobessa@gmail.com>
2010-08-28 00:46:56 -0300

    Short answer when you find the meaning. Closes #2.

--- lib/did-you-mean.rb
+++ lib/did-you-mean.rb
@@ -2,36 +2,38 @@ require 'rubygems'
 require 'levenshtein'
 
 module DidYouMean
+  
+  module ClassMethods
+    def methods_with_a_search
+      methods_array = methods_with_no_search
+      class << methods_array
+        def search(query)
+          self.sort_by {|name| Levenshtein.normalized_distance(name.to_s, query.to_s) }
+        end
+      end
+      methods_array
+    end
+  end
+
   def self.included(base)
     base.class_eval do
-      def methods_with_a_search
-        methods_array = methods_with_no_search
-        class << methods_array
-          def search(query)
-            self.sort_by {|name| Levenshtein.normalized_distance(name.to_s, query.to_s) }
-          end
-        end
-        methods_array
-      end
+      include DidYouMean::ClassMethods
       alias_method :methods_with_no_search, :methods
       alias_method :methods, :methods_with_a_search
 
-      unless method_defined? :method_missing
-        def method_missing(meth, *args, &block); super; end
-      end
       alias_method :method_missing_without_method_search, :method_missing
       alias_method :method_missing, :method_missing_with_method_search
-
     end
+  end  
 
-  end
   def method_missing_with_method_search(name,*args,&block)
-    unless (self.respond_to? name)
+    unless self.respond_to? name
       best_match = self.methods.search(name).first
-      $stdout.print "Did you mean #{self.class}.#{best_match} (yes/no)?\n"
-      response = $stdin.gets
-      return self.send(best_match,*args,&block) if /yes/ =~ response
+      $stdout.print "Did you mean #{self.class}.#{best_match} (Yes/no)?\n"
+      response = $stdin.gets.strip
+      return self.send(best_match,*args,&block) if /\A(yes|y|)\z/i =~ response
     end
-    method_missing_without_method_search name,*args,&block
+    method_missing_without_method_search name,*args,&block 
   end
+
 end 
--- spec/did-you-mean_spec.rb
+++ spec/did-you-mean_spec.rb
@@ -23,21 +23,24 @@ describe "DidYouMean" do
     subject do
       Class.new{include(DidYouMean)}.new
     end
-
-    it "should suggest methods by textual proximity and call it if develepor types yes" do
-      def subject.foo; end
-      $stdout.should_receive(:print).with("Did you mean #{subject.class}.foo (yes/no)?\n")
-      $stdin.should_receive(:gets).and_return('yes')
-      subject.should_receive(:foo)
-      subject.fo
+    ["yes","y",""].each do |answer|
+      it "should suggest methods by textual proximity and call it if develepor types '#{answer}'" do
+        def subject.foo; end
+        $stdout.should_receive(:print).with("Did you mean #{subject.class}.foo (Yes/no)?\n")
+        $stdin.should_receive(:gets).and_return(answer)
+        subject.should_receive(:foo)
+        subject.fo
+      end
     end
-    
-    it "should suggest methods by textual proximity and dont call it if develepor types no" do
-      def subject.foo; end
-      $stdout.should_receive(:print).with("Did you mean #{subject.class}.foo (yes/no)?\n")
-      $stdin.should_receive(:gets).and_return('no')
-      subject.should_not_receive(:foo)
-      lambda {subject.fo}.should raise_error(NoMethodError)      
+
+    ["no","n"].each do |answer|
+      it "should suggest methods by textual proximity and dont call it if develepor types '#{answer}'" do
+        def subject.foo; end
+        $stdout.should_receive(:print).with("Did you mean #{subject.class}.foo (Yes/no)?\n")
+        $stdin.should_receive(:gets).and_return(answer)
+        subject.should_not_receive(:foo)
+        lambda {subject.fo}.should raise_error(NoMethodError)      
+      end
     end
 
   end
